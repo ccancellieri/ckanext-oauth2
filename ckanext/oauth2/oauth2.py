b56a0e2efd61161d073c85fb254936548c24f9a4
carlo cancellieri <ccancellieri@hotmail.com>
2020-10-20 14:50:27 +0200

    fix2

--- ckanext/oauth2/oauth2.py
+++ ckanext/oauth2/oauth2.py
@@ -142,7 +142,7 @@ class OAuth2Helper(object):
         def flatten_dict(d):
             def expand(key, value):
                 if isinstance(value, dict):
-                    return [ (key + '.' + k, v) for k, v in self.flatten_dict(value).items() ]
+                    return [ (key + '.' + k, v) for k, v in flatten_dict(value).items() ]
                 else:
                     return [ (key, value) ]
             items = [ item for k, v in d.items() for item in expand(k, v) ]
