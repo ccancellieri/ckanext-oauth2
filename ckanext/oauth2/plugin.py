d6f51dbb4a6c651c24ccfdf7bfd4b10bae8c5a20
carlo cancellieri <ccancellieri@hotmail.com>
2020-10-20 20:37:10 +0200

    fix!!!5

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -178,7 +178,8 @@ class OAuth2Plugin(plugins.SingletonPlugin):
                     #logout
                     g.user = ''
                     toolkit.c.user = ''
-                    toolkit.redirect_to('/user/login'.encode('utf-8'))
+                    self.oauth2helper.login(_get_previous_page("https://data.review.fao.org/ckan"))
+                    # toolkit.redirect_to('/user/login'.encode('utf-8'))
                     
                     return #TODO temp fix redirect does not works!
                 
@@ -204,6 +205,26 @@ class OAuth2Plugin(plugins.SingletonPlugin):
             g.user = None
             #toolkit.c.user = None #TODO needed?
             log.warn('The user is not currently logged...')
+
+    def _get_previous_page(default_page):
+        if 'came_from' not in toolkit.request.params:
+            came_from_url = toolkit.request.headers.get('Referer', default_page)
+        else:
+            came_from_url = toolkit.request.params.get('came_from', default_page)
+
+        came_from_url_parsed = urlparse(came_from_url)
+
+        # Avoid redirecting users to external hosts
+        if came_from_url_parsed.netloc != '' and came_from_url_parsed.netloc != toolkit.request.host:
+            came_from_url = default_page
+
+        # When a user is being logged and REFERER == HOME or LOGOUT_PAGE
+        # he/she must be redirected to the dashboard
+        pages = ['/', '/user/logged_out_redirect']
+        if came_from_url_parsed.path in pages:
+            came_from_url = default_page
+
+        return came_from_url
 
 
     def get_auth_functions(self):
