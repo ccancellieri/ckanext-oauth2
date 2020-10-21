584bde5b06907df0132504e34643f44f34f8a422
carlo cancellieri <ccancellieri@hotmail.com>
2020-10-21 18:23:06 +0200

    fix & missing

--- ckanext/oauth2/oauth2.py
+++ ckanext/oauth2/oauth2.py
@@ -179,23 +179,22 @@ class OAuth2Helper(object):
         return plugins.get(self.rememberer_name)
 
     def _get_previous_page(default_page):
-    if 'came_from' not in toolkit.request.params:
-        came_from_url = toolkit.request.headers.get('Referer', default_page)
-    else:
-        came_from_url = toolkit.request.params.get('came_from', default_page)
+        if 'came_from' not in toolkit.request.params:
+            came_from_url = toolkit.request.headers.get('Referer', default_page)
+        else:
+            came_from_url = toolkit.request.params.get('came_from', default_page)
+        came_from_url_parsed = urlparse(came_from_url)
 
-    came_from_url_parsed = urlparse(came_from_url)
+        # Avoid redirecting users to external hosts
+        if came_from_url_parsed.netloc != '' and came_from_url_parsed.netloc != toolkit.request.host:
+            came_from_url = default_page
 
-    # Avoid redirecting users to external hosts
-    if came_from_url_parsed.netloc != '' and came_from_url_parsed.netloc != toolkit.request.host:
-        came_from_url = default_page
-
-    # When a user is being logged and REFERER == HOME or LOGOUT_PAGE
-    # he/she must be redirected to the dashboard
-    pages = ['/', '/user/logged_out_redirect']
-    if came_from_url_parsed.path in pages:
-        came_from_url = default_page
-    return came_from_url
+        # When a user is being logged and REFERER == HOME or LOGOUT_PAGE
+        # he/she must be redirected to the dashboard
+        pages = ['/', '/user/logged_out_redirect']
+        if came_from_url_parsed.path in pages:
+            came_from_url = default_page
+        return came_from_url
 
     def remember(self, user_name):
         '''
